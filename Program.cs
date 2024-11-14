8109b974d391382825f85a9895d1dca888afd82e
Priccila Lucem <priccilalucem@gmail.com>
2024-11-14 10:06:35 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,23 +1,38 @@
-using System.Text.Json.Serialization;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using TrilhaApiDesafio.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using DotNetEnv;
+using System;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+var mysqlVersion = new MySqlServerVersion(new Version(8, 0, 33)); 
+var server = Environment.GetEnvironmentVariable("DB_SERVER");
+var database = Environment.GetEnvironmentVariable("DB_DATABASE");
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
 
+var connectionString = $"Server={server};Database={database};User={user};Password={password};";
+
+builder.Services.AddDbContext<OrganizadorContext>(options =>
+    options.UseMySql(
+        connectionString,
+        new MySqlServerVersion(new Version(8, 0, 33)) 
+    )
+);
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
